7894b0567f31429dc367a5790b7d7569d31e9f52
Sawan Ruppala <ruppalasawan@gmail.com>
2025-02-11 17:41:04 -0800

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -22,7 +22,7 @@ import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import threejs from "./tech/threejs.svg";
+import threejs from "./tech/threejs.png";
 import typescript from "./tech/typescript.png";
 
 import meta from "./company/meta.png";
